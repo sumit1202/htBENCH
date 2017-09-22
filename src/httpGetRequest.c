fd9e87bcc03c73007b6e85481524496ac66f253c
Sumit Kumar <31153960+sumit1202@users.noreply.github.com>
2017-09-22 10:39:35 +0530

    fixed the serv_addr cli warning (#3)

--- src/httpGetRequest.c
+++ src/httpGetRequest.c
@@ -49,7 +49,7 @@ int main(int argc, char *argv[])
         server->h_length);
   serv_addr.sin_port = htons(portno);
 
-  if (connect(sockfd, &serv_addr, sizeof(serv_addr)) < 0)
+  if (connect(sockfd,(struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
     error("ERROR connecting");
 
   n = write(sockfd, "GET /\r\n", strlen("GET /\r\n"));
